5f6d0aaa28ba9bfc80ec5205447ca41d8b5d7684
Tzarc <tz@tzarc.org>
2016-09-04 13:40:08 +1000

    Commit for 70000.14.

--- Libs/LibProfiling-1.0.lua
+++ Libs/LibProfiling-1.0.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "LibProfiling-1.0", 13
+local MAJOR, MINOR = "LibProfiling-1.0", 14
 local LibProfiling, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 local type = type
--- Libs/LibTableCache-1.0.lua
+++ Libs/LibTableCache-1.0.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "LibTableCache-1.0", 13
+local MAJOR, MINOR = "LibTableCache-1.0", 14
 local LibTableCache, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 ------------------------------------------------------------------------------------------------------------------------
