abc78f9bb8ed95155658464b010fb340b8348799
Tzarc <tz@tzarc.org>
2016-09-06 16:13:13 +1000

    Commit for 70000.17.

--- Libs/LibTableCache-1.0.lua
+++ Libs/LibTableCache-1.0.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "LibTableCache-1.0", 16
+local MAJOR, MINOR = "LibTableCache-1.0", 17
 local LibTableCache, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 ------------------------------------------------------------------------------------------------------------------------
