c9660817b72518462318e07f9019e29bebe2d5ef
Tzarc <tz@tzarc.org>
2016-09-23 15:51:49 +1000

    Use curse localisation.

--- Lang-enUS.lua
+++ Lang-enUS.lua
@@ -5,14 +5,4 @@ debug = true
 
 local L = LibStub("AceLocale-3.0"):NewLocale("ThousandJabs", "enUS", true, debug)
 
-L["Ability Disabling"] = true
-L["Disable Specialisation"] = true
-L["Fading"] = true
-L["General Settings"] = true
-L["Hide out-of-combat"] = true
-L["In-combat alpha"] = true
-L["Out-of-combat alpha"] = true
-L["Reset Position"] = true
-L["Specialisation Settings"] = true
-L["Toggle Lock"] = true
-L["Window Scale"] = true
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
