9fcd9f4878f0ae38d10275fddf9ed941d82c66b9
Tzarc <tz@tzarc.org>
2016-10-09 01:21:02 +1100

    Commit for 70000.28.

--- Libs/LibProfiling-1.0.lua
+++ Libs/LibProfiling-1.0.lua
@@ -1,5 +1,5 @@
 local addonName = ...
-local MAJOR, MINOR = "LibProfiling-1.0", 27
+local MAJOR, MINOR = "LibProfiling-1.0", 28
 local LibProfiling, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 local type = type
