a3d830a49894aa9acd0cf57b0878e74b7fa090cc
Tzarc <tz@tzarc.org>
2016-10-10 20:39:12 +1100

    Translations.

--- Lang-deDE.lua
+++
@@ -1,8 +0,0 @@
-local debug = false
---[===[@debug@
-debug = true
---@end-debug@]===]
-
-local L = LibStub("AceLocale-3.0"):NewLocale("ThousandJabs", "deDE", true, debug)
-
---@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
--- Lang-enUS.lua
+++
@@ -1,8 +0,0 @@
-local debug = false
---[===[@debug@
-debug = true
---@end-debug@]===]
-
-local L = LibStub("AceLocale-3.0"):NewLocale("ThousandJabs", "enUS", true, debug)
-
---@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
