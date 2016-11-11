1c35f5f9d8405ae9b663d9249b82e7bce8601480
Tzarc <tz@tzarc.org>
2016-11-12 10:25:45 +1100

    Commit for 70100.038.

--- perform-tag.sh
+++ perform-tag.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-bump_lib() {
-    LIB_FILE="$1"
-    LIB_NAME=$(basename "${LIB_FILE}" .lua)
-    CUR_VERSION=$(grep 'local MAJOR, MINOR' "${LIB_FILE}" | cut -d',' -f3 | sed -e 's#[\s\r\n]*##g')
-    NEW_VERSION=$((${CUR_VERSION} + 1))
-    [[ -z ${NEW_VERSION} ]] && echo "Failed to increase version." && exit 1
-    sed -i "s#local MAJOR, MINOR = \"${LIB_NAME}\", \([0-9]\+\)#local MAJOR, MINOR = \"${LIB_NAME}\", ${NEW_VERSION}#" "${LIB_FILE}"
-}
-
 if [[ -z $1 ]] ; then
-    LATEST_TAG=$(git tag | grep -P '[0-9]{5}\.[0-9]{3}' | sort)
+    LATEST_TAG=$(git tag | grep -P '[0-9]{5}\.[0-9]{3}' | sort | tail -n1)
     TAG_HI=$(echo $LATEST_TAG | cut -d'.' -f1)
     TAG_LO=$(echo $LATEST_TAG | cut -d'.' -f2 | sed -e 's#^0*##g')
     TAG_LO=$(($TAG_LO + 1))
@@ -18,10 +9,6 @@ if [[ -z $1 ]] ; then
 else
     NEW_TAG=$1
 fi
-
-bump_lib Libs/LibProfiling-1.0.lua
-bump_lib Libs/LibTableCache-1.0.lua
-bump_lib Libs/LibUnitCache-1.0.lua
 
 # Update the Version: tag in the toc
 sed -i "s#Version: \([v0-9\.]\+\)#Version: ${NEW_TAG}#" ThousandJabs.toc
