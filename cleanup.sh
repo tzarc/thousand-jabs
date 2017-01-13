d89329a706ca0ed88ce06974614a5e2d7b28fe48
Tzarc <tz@tzarc.org>
2017-01-13 12:41:33 +1100

    Parallelise cleanup, ensure it's run as a git pre-commit hook.

--- cleanup.sh
+++ cleanup.sh
@@ -2,31 +2,37 @@
 
 set -e
 
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+cd "${SCRIPT_DIR}"
+
 tjfind() {
     find . \( "$@" \) -and -not -path './.git/*' -and -not -path '*__pycache__*' -and -not -path '*PythonEnvironment*' -and -not -path './simc*' -and -not -path './Temp*' -print | sort
 }
 
-for file in $(tjfind -type f) ; do
-    chmod -x "${file}"
-done
+# Remove executable flag on all files
+tjfind -type f | parallel "chmod -x '{1}'"
 
-for file in $(tjfind -iname '*.toc' -or -iname '*.lua' -or -iname '*.sh' -or -iname '*.py' -or -iname '*.simc') ; do
-    dos2unix $file >/dev/null 2>&1
-    chmod -x $file >/dev/null 2>&1
-    sed -i 's/[ \t]*$//' $file
-    sed -i 's/^local devMode = true/local devMode = false/' $file
-done
+# Remove trailing whitespace
+tjfind -iname '*.toc' -or -iname '*.lua' -or -iname '*.sh' -or -iname '*.py' -or -iname '*.simc' | parallel "sed -i 's/[ \t]*\$//' '{1}'"
 
-for file in $(tjfind -iname '*.lua') ; do
-    echo "Formatting '${file}'..."
-    luaformatter -a -s4 "${file}"
-done
+# Disable devMode
+tjfind -iname '*.lua' | parallel "sed -i 's/^local devMode = true/local devMode = false/' '{1}'"
 
-for file in $(tjfind -iname '*.sh' -or -iname '*.pl') ; do
-    dos2unix $file >/dev/null 2>&1
-    chmod +x $file >/dev/null 2>&1
-done
+# Reformat lua files
+tjfind -iname '*.lua' | parallel "echo \"Formatting '{1}'\" && luaformatter -a -s4 '{1}'"
 
-for file in $(tjfind -iname '*.pl') ; do
-    perltidy -b -pt=0 -l=200 "${file}"
-done
+# Reformat perl scripts
+tjfind -iname '*.pl' | parallel "perltidy -pt=0 -l=200 '{1}' && cat '{1}.tdy' > '{1}' && rm '{1}.tdy'"
+
+# Make sure everything has Unix line endings
+tjfind -iname '*.toc' -or -iname '*.lua' -or -iname '*.sh' -or -iname '*.pl' -or -iname '*.simc' | parallel "dos2unix '{1}' >/dev/null 2>&1"
+
+# Make sure scripts are executable
+tjfind -iname '*.sh' -or -iname '*.pl' | parallel "chmod +x '{1}'"
+
+if [[ ! -L "${SCRIPT_DIR}/.git/hooks/pre-commit" ]] ; then
+    (cd "${SCRIPT_DIR}/.git/hooks" && ln -sf ../../cleanup.sh pre-commit)
+fi
+
+[[ ! -z "$(git diff)" ]] && echo -e "\e[0;37m[\e[1;31mFAIL\e[0;37m]\e[0m Aborting commit: 'git diff' says changes are still present." && exit 1
+exit 0
